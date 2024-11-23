724faa398a5801f28934e620e8919c8ac3de8781
ayrton <ayrtonalexanderupp@gmail.com>
2024-11-23 20:33:21 -0300

    version 2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-//Nav
-const themeToggle = document.getElementById('themeToggle');
-themeToggle.addEventListener('change', () => {
-    document.body.classList.toggle('dark-theme');
-});
-
-
-
-
-
 // carrousel
 const items = document.querySelectorAll('.carousel-item');
 const dots = document.querySelectorAll('.dot');
